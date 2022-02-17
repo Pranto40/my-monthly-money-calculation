a280af773657fdfc3b5b8d930e9b169f2a553b54
pranto <polasahammedpranto@gmail.com>
2022-02-17 13:46:32 +0600

    calculate a saving amount

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 window.onload  = () => {
     main ();
 };
-
-function main () {
+    let income = parseInt(document.getElementById('income').value);
     let calculatebtn = document.getElementById('calculate');
-    let food = parseFloat(document.getElementById('food').value);
-    let rent = parseFloat(document.getElementById('rent').value);
-    let clothes = parseFloat(document.getElementById('clothes').value);
-    
+    let food = parseInt(document.getElementById('food').value);
+    let rent = parseInt(document.getElementById('rent').value);
+    let clothes = parseInt(document.getElementById('clothes').value);
+
+    // calculation a total balance
+    let totalBlance = income - (food + rent + clothes);
+
+    // calculate button addEventListener handle and function decleare
+function main () {
+     
     calculatebtn.addEventListener('click', function () {
-        
+        // error condition
         if (food < 0){
             let foodError = document.getElementById('food-error');
             foodError.innerHTML = 'please enter the positive number on the food';
-            
         }
-        
         else if (rent < 0) {
             let foodError = document.getElementById('rent-error');
             foodError.innerHTML = 'please enter the positive number on the rent';
@@ -25,16 +28,34 @@ function main () {
             foodError.innerHTML = 'please enter the positive number on the clothes';
         }
         else {
+            // calculation a total expense
             let totalExpense = food + rent + clothes;
             document.getElementById('total-expense').innerHTML = totalExpense;
 
-        let income = parseFloat(document.getElementById('income').value);
-        let totalBlance = income - (food + rent + clothes);
-        document.getElementById('balance').innerHTML = totalBlance;
-        }
+            let income = parseInt(document.getElementById('income').value);
+            document.getElementById('balance').innerHTML = totalBlance;
+            if (income < 0) {
+                let incomeError = document.getElementById('income-error');
+                incomeError.innerHTML = 'please enter the positive number on the income';
+            }
+            else if (income < totalExpense) {
+                let balanceError = document.getElementById('balance-error');
+                balanceError.innerHTML = 'your amount low';
+                }
+            }
     });
 
 };
+// save button addEventListener handle and function decleare
+document.getElementById('saveButton').addEventListener('click', function () {
+    let persentesValue = parseInt(document.getElementById('persentes-value').value);
+    let calculateValue = income / persentesValue;
+    document.getElementById('saving-amount').innerHTML = calculateValue;
+
+    let remainingBalance = totalBlance - calculateValue;
+    document.getElementById('remaining-balance').innerHTML = remainingBalance;
+
+});
 
 
 
