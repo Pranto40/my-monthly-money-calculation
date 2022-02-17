e1201bc2cc265b5f08c30d06640bbc465218b292
pranto <polasahammedpranto@gmail.com>
2022-02-17 21:52:23 +0600

    modify to javascript file

--- js/main.js
+++ js/main.js
@@ -1,67 +1,59 @@
 
-window.onload  = () => {
-    main ();
-};
-    let income = parseInt(document.getElementById('income').value);
-    let food = parseInt(document.getElementById('food').value);
-    let rent = parseInt(document.getElementById('rent').value);
-    let clothes = parseInt(document.getElementById('clothes').value);
-    let calculatebtn = document.getElementById('calculate');
-    
-    // calculation a total balance
-    let totalBlance = income - (food + rent + clothes);
+    document.getElementById('calculate').addEventListener('click', function () {
+        let food = parseInt(document.getElementById('food').value);
+        let rent = parseInt(document.getElementById('rent').value);
+        let clothes = parseInt(document.getElementById('clothes').value);
 
-    // calculate button addEventListener handle and function decleare
-function main () {
-    calculatebtn.addEventListener('click', function () {
-        // error condition
+        //  calculation a total expense
+        let totalExpense = food + rent + clothes;
+        document.getElementById('total-expense').innerHTML = totalExpense;
+
+        let income = parseInt(document.getElementById('income').value);
+        let totalBlance = income - (food + rent + clothes);
+        document.getElementById('balance').innerHTML = totalBlance;
+        // condition declear
+        if (isNaN(income) || income < 0) {
+            let incomeError = document.getElementById('income-error');
+            incomeError.innerHTML = 'please enter the number on the income';
+        }
+        else if (income < totalExpense) {
+        let balanceError = document.getElementById('balance-error');
+        balanceError.innerHTML = 'You have overcharged';
+        }
+
         if (isNaN(food) || food < 0){
-            let foodError = document.getElementById('food-error');
-            foodError.innerHTML = 'please enter the number on the food';
+        let foodError = document.getElementById('food-error');
+        foodError.innerHTML = 'please enter the number on the food';
         }
         else if (isNaN(rent) || rent < 0) {
-            let foodError = document.getElementById('rent-error');
-            foodError.innerHTML = 'please enter the number on the rent';
+        let foodError = document.getElementById('rent-error');
+        foodError.innerHTML = 'please enter the number on the rent';
         }
         else if (isNaN(clothes) || clothes < 0) {
-            let foodError = document.getElementById('clothes-error');
-            foodError.innerHTML = 'please enter the number on the clothes';
+        let foodError = document.getElementById('clothes-error');
+        foodError.innerHTML = 'please enter the number on the clothes';
         }
-        else {
-            // calculation a total expense
-            let totalExpense = food + rent + clothes;
-            document.getElementById('total-expense').innerHTML = totalExpense;
+    });   
 
-            let income = parseInt(document.getElementById('income').value);
-            document.getElementById('balance').innerHTML = totalBlance;
-            if (isNaN(income) || income < 0) {
-                let incomeError = document.getElementById('income-error');
-                incomeError.innerHTML = 'please enter the number on the income';
-            }
-            else if (income < totalExpense) {
-                let balanceError = document.getElementById('balance-error');
-                balanceError.innerHTML = 'You have overcharged';
-                }
-            }
-    });
-
-};
-// save button addEventListener handle and function decleare
-document.getElementById('saveButton').addEventListener('click', function () {
-    let persentesValue = parseInt(document.getElementById('persentes-value').value);
-    let calculateValue = income * (persentesValue/100);
-    document.getElementById('saving-amount').innerHTML = calculateValue;
-
-    let remainingBalance = totalBlance - calculateValue;
-    document.getElementById('remaining-balance').innerHTML = remainingBalance;
-
-    if (totalBlance < calculateValue) {
-        let savingError = document.getElementById('saving-error');
-        savingError.innerHTML = 'your balance low you not saving money';
-    }
-});
-
-
-
-
+// saving buttan claculate
+    document.getElementById('saveButton').addEventListener('click', function () {
+        let income = parseInt(document.getElementById('income').value);
+        let food = parseInt(document.getElementById('food').value);
+        let rent = parseInt(document.getElementById('rent').value);
+        let clothes = parseInt(document.getElementById('clothes').value);
+        // Saving amount claculate
+        let persentesValue = parseInt(document.getElementById('persentes-value').value);
+        let calculateValue = income * (persentesValue/100);
+        document.getElementById('saving-amount').innerHTML = calculateValue;
+        // Remaining Balance claculate
+        let totalBlance = income - (food + rent + clothes);
+        let remainingBalance = totalBlance - calculateValue;
+        document.getElementById('remaining-balance').innerHTML = remainingBalance;
+         // Saving error 
+        if (totalBlance < calculateValue) {
+         let savingError = document.getElementById('saving-error');
+         savingError.innerHTML = 'your balance low you not saving money';
+        }
+ });
+ 
 
